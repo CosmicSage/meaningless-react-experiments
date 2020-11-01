9cd030c68edbae1a454be0f132de2e22a31115da
CosmicSage <prabhakarmishra9@yahoo.com>
2020-11-02 02:33:36 +0530

    BlowUP

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import { ReactComponent as SmallArrow } from "./arrow.svg";
-import { useEffect } from "react";
 
 const Checkbox = props => (
-  <div onClick={() => {
-    props.one(props.value)
-  }}>
+  <div onClick={() => {props.one(props.value)}}>
     <input type="checkbox" name={props.value} className="check"/>
     <label for={props.value}>{props.value}</label>
   </div>
 )
 
+function blow() {
+  let main = document.getElementById("box_body");
+  main.remove()
+}
 
 function App() {
   const onlyOne = (checkbox) => {
@@ -27,29 +28,21 @@ function App() {
     document.getElementById("box_control").style.transform = document.getElementById("box_control").style.transform === "rotateX(180deg)" ? null : "rotateX(180deg)";
   }
 
-  (async () => {
-    await new Promise(r => setTimeout(r, 2000));
-    document.title = "youu"
-  })();
-
   return (
-    <>
-      <div id="box_body" className="box_body">
-        <div id="box_control" className="box_control" onClick={minimize}>
-          <SmallArrow/>
+    <div id="box_body" className="box_body">
+      <div id="box_control" className="box_control" onClick={minimize}>
+        <SmallArrow/>
+      </div>
+      <div id="box_content">
+        <p >Did you find the website helpful?</p>
+        <div id="box_options">
+          {["Yes", "No"].map(value => <Checkbox value={value} one={onlyOne}/>)}
         </div>
-        <div id="box_content">
-          <p >Did you find the website helpful?</p>
-          <div id="box_options">
-            {["Yes", "No"].map(value => <Checkbox value={value} one={onlyOne}/>)}
-          </div>
-          <div style={{paddingTop : 50, display: "flex", justifyContent : "flex-end"}}>
-            <button>Submit</button>
-          </div>
-
+        <div style={{paddingTop : 50, display: "flex", justifyContent : "flex-end"}}>
+          <button onClick={blow} className="box_custom_button">Submit</button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
