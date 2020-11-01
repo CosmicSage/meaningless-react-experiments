a93353a7755b504ca90a79c5c960417b0fc394c7
CosmicSage <prabhakarmishra9@yahoo.com>
2020-11-01 22:25:01 +0530

    Mutiple Choice -- Design complete

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 import { ReactComponent as SmallArrow } from "./arrow.svg";
+import { useEffect } from "react";
+
+const Checkbox = props => (
+  <div onClick={() => {
+    props.one(props.value)
+  }}>
+    <input type="checkbox" name={props.value} className="check"/>
+    <label for={props.value}>{props.value}</label>
+  </div>
+)
+
+
 function App() {
+  const onlyOne = (checkbox) => {
+    var checkboxes = document.getElementsByClassName('check');
+    console.log(checkbox);
+    [...checkboxes].forEach((item) => {
+        if (item.name !== checkbox) item.checked = false
+    })
+  }
+
   const minimize = () => {
+    // Add condition remove condition
     document.getElementById("box_body").style.height = document.getElementById("box_body").style.height === "10px" ? null : "10px";
     document.getElementById("box_control").style.transform = document.getElementById("box_control").style.transform === "rotateX(180deg)" ? null : "rotateX(180deg)";
   }
+
+  (async () => {
+    await new Promise(r => setTimeout(r, 2000));
+    document.title = "youu"
+  })();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div id="box_body" className="box_body">
+    <>
+      <div id="box_body" className="box_body">
         <div id="box_control" className="box_control" onClick={minimize}>
           <SmallArrow/>
         </div>
+        <div id="box_content">
+          <p >Did you find the website helpful?</p>
+          <div id="box_options">
+            {["Yes", "No"].map(value => <Checkbox value={value} one={onlyOne}/>)}
+          </div>
+          <div style={{paddingTop : 50, display: "flex", justifyContent : "flex-end"}}>
+            <button>Submit</button>
+          </div>
 
-        <p>logogo</p>
         </div>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
