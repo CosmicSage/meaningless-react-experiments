84007c460ed1993ef673612dd4b1a815c21a07c6
CosmicSage <prabhakarmishra9@yahoo.com>
2020-11-02 22:59:25 +0530

    Big Boy Bundle with Billys of loaders

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    entry: "./src/App.js",
+    output: {
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: { loader: 'babel-loader' }
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                  {
+                    loader: 'svg-url-loader',
+                    options: {
+                      limit: 10000,
+                    },
+                  },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [{loader: 'css-loader'}]
+            },
+        ]
+    }
+};
